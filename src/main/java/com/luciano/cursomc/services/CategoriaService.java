d604a1f6401ecd955618b161172ed0e6b1523f30
Luciano Goncalves <lucianogoa@gmail.com>
2020-10-26 15:12:54 -0300

    Criado servico e repositorio

---
+++ src/main/java/com/luciano/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.luciano.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luciano.cursomc.domain.Categoria;
+import com.luciano.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria Buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+}
