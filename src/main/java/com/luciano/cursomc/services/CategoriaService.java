ce22c397acc19f165bd1aeabddf47b3b72411177
Luciano Goncalves <lucianogoa@gmail.com>
2020-10-26 18:01:56 -0300

    Ajustes no endpoint catecorias;{id}

--- src/main/java/com/luciano/cursomc/services/CategoriaService.java
+++ src/main/java/com/luciano/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.luciano.cursomc.domain.Categoria;
 import com.luciano.cursomc.repositories.CategoriaRepository;
+import com.luciano.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria Buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		
 	}
 }
