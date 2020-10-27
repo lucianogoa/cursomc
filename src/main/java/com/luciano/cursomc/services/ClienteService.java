b9e68252e172a7f90bbcb3854a07f1b5473f9b0e
Luciano Goncalves <lucianogoa@gmail.com>
2020-10-27 13:38:00 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/luciano/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.luciano.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luciano.cursomc.domain.Cliente;
+import com.luciano.cursomc.repositories.ClienteRepository;
+import com.luciano.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente Buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+}
