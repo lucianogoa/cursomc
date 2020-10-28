7eb38da373f770f3099c6861bdf7e8058ddd1e91
Luciano Goncalves <lucianogoa@gmail.com>
2020-10-28 11:30:41 -0300

    Inserindo uma nova Categoria via POST

--- src/main/java/com/luciano/cursomc/resources/ClienteResource.java
+++ src/main/java/com/luciano/cursomc/resources/ClienteResource.java
@@ -20,9 +20,9 @@ public class ClienteResource {
 	private ClienteService service;
 	
 	@RequestMapping(value="/{id}" , method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		
-		Cliente obj = service.Buscar(id);
+		Cliente obj = service.find(id);
 		
 		return ResponseEntity.ok(obj);
 	}
--- src/main/java/com/luciano/cursomc/services/CategoriaService.java
+++ src/main/java/com/luciano/cursomc/services/CategoriaService.java
@@ -15,10 +15,15 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria Buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }
